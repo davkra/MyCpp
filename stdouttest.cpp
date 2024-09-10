a11488a739cad91e0a737986cd954cfe46385bcb
David Krall <davkra@chello.at>
2024-09-10 03:12:37 +0200

    added stdouttest

---
+++ stdouttest.cpp
@@ -0,0 +1,40 @@
+#include <fstream>
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char const *argv[]) {
+  std::cout << "Debug Test" << std::endl;
+
+  // Backup original stdout
+  FILE *original_stdout = fdopen(dup(fileno(stdout)), "w");
+
+  if (freopen("temp_output.txt", "w", stdout)) {
+    printf("Hello world!\n");
+
+    // Flush the output to the file, but don't close stdout
+    fflush(stdout);
+
+    // Restore stdout to the original stream
+    dup2(fileno(original_stdout), fileno(stdout));
+    fclose(original_stdout); // Close the copy of stdout
+
+  } else {
+    std::cout << "ERROR!" << std::endl;
+    return -1;
+  }
+
+  // Read and print the file content
+  std::ifstream f("temp_output.txt");
+  if (f.is_open()) {
+    std::cout << "file is open" << std::endl;
+    std::cout << f.rdbuf();
+  } else {
+    std::cerr << "Error opening the file!" << std::endl;
+  }
+
+  // Now printf and cout will work again
+  printf("Hi!\n");
+  return 0;
+}
