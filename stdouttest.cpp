d4e1f28f6f52974a0b00551a4bc2b2e27be13578
David Krall <davkra@chello.at>
2024-09-10 15:14:44 +0200

    convert file to string

--- stdouttest.cpp
+++ stdouttest.cpp
@@ -1,6 +1,7 @@
 #include <fstream>
 #include <iostream>
 #include <unistd.h>
+#include <sstream>
 
 void test_function(void)
 {
@@ -30,19 +31,34 @@ public:
 
   void switch_stdout_to_console(std::string file_name)
   {
+    fflush(stdout);
+    dup2(fileno(original_stdout), fileno(stdout));
+    fclose(original_stdout);
   }
 };
 
-void print_file(std::string file_name)
+void switch_stdout_to_console(FILE *original_stdout)
+{
+  fflush(stdout);
+  dup2(fileno(original_stdout), fileno(stdout));
+  fclose(original_stdout);
+}
+
+std::string read_and_remove_file(std::string file_name)
 {
   std::ifstream f(file_name);
   if (f.is_open())
   {
-    std::cout << f.rdbuf();
+    std::stringstream buffer;
+    buffer << f.rdbuf();
+    remove(file_name.c_str());
+
+    return buffer.str();
   }
   else
   {
     std::cout << "Error opening the file!" << std::endl;
+    return nullptr;
   }
 }
 
@@ -52,7 +68,9 @@ int main(int argc, char const *argv[])
   // switchStandout.switch_stdout_to_file("temp_output.txt");
 
   FILE *original_stdout = fdopen(dup(fileno(stdout)), "w");
-  if (!freopen("temp_output.txt", "w", stdout))
+  std::string filename = "temp_output.txt";
+
+  if (!freopen(filename.c_str(), "w", stdout))
   {
     std::cout << "ERROR!" << std::endl;
     return -1;
@@ -64,11 +82,13 @@ int main(int argc, char const *argv[])
   fprintf(stdout, "fprint\n");
   //
 
-  fflush(stdout);
-  dup2(fileno(original_stdout), fileno(stdout));
-  fclose(original_stdout);
+  switch_stdout_to_console(original_stdout);
 
-  print_file("temp_output.txt");
+  // save "temp_output.txt" in string
+  std::string output = read_and_remove_file("temp_output.txt");
+
+  // string can be changed or printed
+  std::cout << output;
 
   return 0;
 }
