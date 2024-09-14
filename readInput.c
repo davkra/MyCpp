81bc9776b7b1f1ea1e9f68b726e9216d01dfbc34
David Krall <davkra@chello.at>
2024-09-14 20:47:42 +0200

    added readInput

---
+++ readInput.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+  printf("Hello world!\n");
+
+  char buffer[400];
+  char c = 0;
+
+  size_t ctr = 0;
+  while (c != '\n')
+  {
+    c = getchar();
+    buffer[ctr++] = c;
+  }
+  buffer[ctr+1] = '\0';
+
+  printf("=============\n");
+  printf("%s\n", buffer);
+
+  return 0;
+}
