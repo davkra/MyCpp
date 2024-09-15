07e7cdb720343db86a5157875a8e0b1b14484824
David Krall <davkra@chello.at>
2024-09-16 01:58:01 +0200

    added implementation for running other binary files

--- bin/run.cpp
+++ bin/run.cpp
@@ -1,8 +1,30 @@
 #include <iostream>
+#include <string.h>
+
+#include <print.hpp>
+
+using my::print;
 
 int main(int argc, char const *argv[])
 {
-  std::system("hello");
+  if (argc == 2)
+  {
+    std::system(argv[1]);
+    return 0;
+  }
+  else if ((argc == 3) && (strcmp(argv[2], "valgrind") == 0))
+  {
+    std::string str("valgrind ");
+    str.append(argv[1]);
 
-  return 0;
+    std::system(str.c_str());
+    return 0;
+  }
+  else
+  {
+    print("Error!");
+    exit(EXIT_FAILURE);
+
+    return 0;
+  }
 }
