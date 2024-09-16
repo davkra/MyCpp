123e3a4cb32f8c3b0b947c5ace28972586101801
David Krall <davkra@chello.at>
2024-09-17 00:37:12 +0200

    compiling and running also with valgrind parameter

--- bin/run.cpp
+++ bin/run.cpp
@@ -2,26 +2,46 @@
 #include <string.h>
 
 #include <print.hpp>
+#define ALWAYS                                                                 \
+  "clear && cmake . && make && echo "                                          \
+  " && "
 
 using my::print;
 
 int main(int argc, char const *argv[])
 {
+  std::string str(ALWAYS);
+
+  print("argc: ", argc);
+
+  for (int ctr = 0; ctr < argc; ctr++)
+  {
+    print("argv[", ctr, "]:", argv[ctr]);
+  }
+
   if (argc == 2)
   {
-    std::system(argv[1]);
+    print("Case 1");
+
+    str.append(argv[1]);
+    print(str);
+    std::system(str.c_str());
     return 0;
   }
   else if ((argc == 3) && (strcmp(argv[2], "valgrind") == 0))
   {
+    print("Case 2");
+
     std::string str("valgrind ");
     str.append(argv[1]);
-
+    print(str);
     std::system(str.c_str());
     return 0;
   }
   else
   {
+    print("Case 3");
+
     print("Error!");
     exit(EXIT_FAILURE);
 
